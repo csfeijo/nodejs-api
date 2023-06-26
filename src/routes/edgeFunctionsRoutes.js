136cee54b42b547a4de3e1e7d19a0917237aa979
Cicero, Feijo <cicero.feijo@gmail.com>
2023-06-26 09:48:43 -0300

    Update Edge Functions to return by ID

--- src/routes/edgeFunctionsRoutes.js
+++ src/routes/edgeFunctionsRoutes.js
@@ -2,17 +2,17 @@ import axios from 'axios'
 
 const edgeFunctionsRoutes = (app) => {
 
-  app.get('/edge-functions', (req, res) => {
-    const options = {
-      headers: {
-        'Accept': 'application/json; version=3',
-        'Authorization': `Token ${process.env.PERSONAL_TOKEN}`
-      }}
+  const options = {
+    headers: {
+      'Accept': 'application/json; version=3',
+      'Authorization': `Token ${process.env.PERSONAL_TOKEN}`
+    }
+  }
 
+  app.get('/edge-functions', (req, res) => {
+    
     axios.get('https://api.azionapi.net/edge_functions?page_size=200', options)
       .then(response => {
-
-        //console.log(response.data)
 
         const results = response.data.results
         const filter = results.map((fun) => {
@@ -29,8 +29,23 @@ const edgeFunctionsRoutes = (app) => {
             
           }
         })
-        console.log(filter)
+        
         res.json(filter)
+        return
+      })
+      .catch(error => {
+        res.status(500).send(`ERROR: ${error}`)
+        console.error(error)
+      })
+  })
+
+  app.get('/edge-functions/:id', (req, res) => {    
+    const { id } = req.params
+
+    axios.get(`https://api.azionapi.net/edge_functions/${id}`, options)
+      .then(response => {
+        
+        res.json(response.data)
         return
       })
       .catch(error => {
