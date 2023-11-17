2d644e013558d185b5e31c4541f4daf53699a7ee
Cicero, Feijo <cicero.feijo@gmail.com>
2023-11-16 23:46:03 -0300

    fix: merged filter

--- src/routes/gitHubChangelogRoutes.js
+++ src/routes/gitHubChangelogRoutes.js
@@ -2,14 +2,22 @@ import axios from 'axios'
 
 const gitHubChangelogRoutes = (app) => {
 
-  const options = {
-    headers: {
-      'Accept': 'application/vnd.github.v3+json',
-      'Authorization': `Token ${process.env.GITHUB_TOKEN}`
-    }
-  }
 
   app.get('/pk-changelog', (req, res) => {
+
+    const options = {
+      headers: {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': `Token ${process.env.GITHUB_TOKEN}`
+      },
+      params: {
+        state: 'closed',
+        'is:merged': true,
+        per_page: 100
+      },
+    }
+  
+
     const { startPeriod } = req.query
     const perPage = 100
     const deployLabel = 'DEPLOY'
@@ -20,18 +28,25 @@ const gitHubChangelogRoutes = (app) => {
       return
     }
 
+    // const gitHubURL = 'https://api.github.com/repos/aziontech/azion-platform-kit/pulls'
+    // let filter = `?state=closed&sort=merged&direction=desc&per_page=${perPage}`
+    // filter += `?q=is%3Apr+is%3Amerged+merged>${startPeriod}+-label%3A${deployLabel}+`
     const gitHubURL = 'https://api.github.com/repos/aziontech/azion-platform-kit/pulls'
-    let filter = `?state=closed&sort=merged&direction=desc&per_page=${perPage}`
-    filter += `?q=is%3Apr+is%3Amerged+merged>${startPeriod}+-label%3A${deployLabel}+`
+    
 
-
-    axios.get(gitHubURL + filter, options)
+    axios.get(gitHubURL, options)
       .then(response => {
 
         const pullRequests = response.data
+      
         const filtered = pullRequests
-          .filter(pr => !pr.labels.some(label => label.name === 'DEPLOY') && new Date(pr.merged_at) >= new Date(startPeriod))
-          .map(pr => `${pr.title}`)
+          .filter(pr => new Date(pr.merged_at) > new Date(startPeriod))
+          .map((pr,index) => {
+
+            return `${index} -> ${pr.merged_at}  - ${pr.title}` 
+          })
+
+
 
         const htmlFiltered = filtered.join('<br/>')
 
